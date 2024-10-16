c6b8e6b88dbb8201a807033054169c4fc7f554b7
xanthospap <xanthos.papanikolaou@gmail.com>
2024-10-17 00:29:42 +0300

    minor

--- bin/orbit3d.py
+++ bin/orbit3d.py
@@ -31,6 +31,10 @@ parser.add_argument("--ref-frame",
     default = 'ecef',
     choices=['ecef', 'eci'],
     help='Reference frame to plot the orbit at. Choose between \'ecef\' or \'eci\'')
+parser.add_argument("--2d", 
+    dest='plot2d',
+    action='store_true',
+    help='If set to true, the program will also produce 2D plots (i.e. time-vs-param) for position and velocity components.')
 
 def main():
 
@@ -50,17 +54,23 @@ def main():
 
 # collect cartesian coordinates and epochs (t).
 # if we are plotting eci, we need to transform the coordinates.
-    t=[];x=[];y=[];z=[];
+    t=[];x=[];y=[];z=[];vx=[];vy=[];vz=[];
     for k,v in data.items():
 # we are only plotting one day of orbit
         if (k-sp3.t_start).total_seconds() <= 86400e0:
             t.append(k)
             if ref_frame == 'ecef':
                 x.append(v['x']); y.append(v['y']); z.append(v['z']); 
+                if args.plot2d:
+                    vx.append(v['vx']); vy.append(v['vy']); vz.append(v['vz']); 
             else:
                 R = Rotation.from_euler('z', gast.approx_gast(k), degrees=False)
                 eci = np.array((v['x'], v['y'], v['z'])) @ R.as_matrix()
                 x.append(eci[0]); y.append(eci[1]); z.append(eci[2]);
+                if args.plot2d:
+                    veci =  R.as_matrix() @ np.array((v['vx'], v['vy'], v['vz'])) + \
+                            np.cross(np.array([0, 0, 7.292115e-5]), np.array((v['x'], v['y'], v['z'])))
+                    vx.append(veci[0]); vy.append(veci[1]); vz.append(veci[2]);
     
     fig = plt.figure()
     ax = plt.axes(projection='3d')
@@ -71,6 +81,18 @@ def main():
     ax.set_ylabel('Y', labelpad=10)
     ax.set_zlabel('Z', labelpad=10)
     plt.show()
+        
+    if args.plot2d:
+        fig, axs = plt.subplots(3,2)
+        fig.suptitle(' [m]')
+        axs[0,0].scatter(t,  x, alpha=.3, edgecolors='none', label=r"$X$")
+        axs[1,0].scatter(t,  y, alpha=.3, edgecolors='none', label=r"$Y$")
+        axs[2,0].scatter(t,  z, alpha=.3, edgecolors='none', label=r"$Z$")
+        axs[0,1].scatter(t, vx, alpha=.3, edgecolors='none', label=r"$v_{x}$")
+        axs[1,1].scatter(t, vy, alpha=.3, edgecolors='none', label=r"$v_{y}$")
+        axs[2,1].scatter(t, vz, alpha=.3, edgecolors='none', label=r"$v_{z}$")
+        plt.legend()
+        plt.show()
 
 if __name__ == "__main__":
     main()
