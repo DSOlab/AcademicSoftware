0603658dedcc83a3d6cf59530bac63112e17a8c0
xanthospap <xanthos.papanikolaou@gmail.com>
2024-11-19 10:39:06 +0200

    minor

--- dsoclasses/time/pyattotime.py
+++ dsoclasses/time/pyattotime.py
@@ -6,3 +6,10 @@ def at2pt(at):
         Translate an attotime instance to a native python datetime instance.
     """
     return datetime.datetime(at.year, at.month, at.day, at.hour, at.minute, at.second, at.microsecond)
+
+def fsec2asec(fsec):
+    isec = int(fsec) # integral seconds
+    imsec = int((fsec - isec)*1e6)  # integral microseconds
+    fnsec = float(fsec*1e9-imsec*1e3) # fractional nanoseconds
+    assert abs(float(attotime.attotimedelta(seconds=isec, microseconds=imsec, nanoseconds=fnsec).total_nanoseconds()) - fsec*1e9) < 1e-1
+    return attotime.attotimedelta(seconds=isec, microseconds=imsec, nanoseconds=fnsec)
