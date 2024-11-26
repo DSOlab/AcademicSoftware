547f83d06bb81afce06b5d53acc23f3b67adcb43
xanthospap <xanthos.papanikolaou@gmail.com>
2024-11-26 03:40:46 +0200

    triti

--- dsoclasses/time/gast.py
+++ dsoclasses/time/gast.py
@@ -8,7 +8,7 @@ def approx_gast(t):
     sec_day = (t - t.replace(hour=0, minute=0, second=0, microsecond=0)).\
                 total_seconds()
     omega_earth = 7.292115e-5 ## [rad/s]
-    return omega_earth * sec_day
+    return omega_earth * float(sec_day)
 
 def R3(theta):
     r = R.from_euler('z', theta, degrees=False).as_matrix()
